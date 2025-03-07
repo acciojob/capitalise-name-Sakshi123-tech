a045b4cde8333f516800635957ce745429b12fb3
Sakshi123-tech <sakshiagnihotri292@gmail.com>
2025-03-07 23:26:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-//your JS code here. If required.
+function capitalizeName() {
+            const inputField = document.getElementById("fname");
+            inputField.value = inputField.value.toUpperCase();
+        }
